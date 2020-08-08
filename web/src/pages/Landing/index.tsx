3fb72886a1bd204d22b691fc287cbf015a3aeabe
Willian Dantas <williansilva1991@gmail.com>
2020-08-08 14:43:21 -0300

    nextlevelweek 2 routes e pages

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
 import landiImg from '../../assets/images/landing.svg';
@@ -13,20 +14,22 @@ function Landing() {
 
     return (
         <div id="page-landing">
-            <div id="pag-landing-content" className="container">
+            <div id="page-landing-content" className="container">
                 <div className="logo-conteiner">
                     <img src={logoImg} alt="Proffy"/>
                     <h2>Sua plataforma de estudos online.</h2>
                 </div>
                 <img src={landiImg} alt="Plataforma de estudos" className="hero-image"/>
                 <div className="buttons-container">
-                    <a href="" className="study">
+                    <Link to="/study" className="study">
                             <img src={studyIcon} alt="Estudar"/>
-                    </a>
+                            Estudar
+                    </Link>
 
-                    <a href="" className="give-classes">
-                            <img src={giveClassesIcon} alt="Estudar"/>
-                    </a>
+                    <Link to="/give-classes" className="give-classes">
+                            <img src={giveClassesIcon} alt="Dar aulas"/>
+                            Dar aulas
+                    </Link>
                 </div>
                 <span className="total-connections">
                     Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
